826e31e930a30ef62fb1e96fa41368d21d23badf
Adrianita13 <adriattar@hotmail.com>
2021-09-06 19:28:44 -0300

    hasta acá funciona subiendo las historias en ul

--- src/components/Aventura.jsx
+++ src/components/Aventura.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
 import HistoriaItem from "./HistoriaItem";
+import data from "./data.json";
 
 class Aventura extends React.Component{
     constructor(props){
         super(props);
         this.state={
-
+            historiaCompleta: []
         }
+        this.agregarItem = this.agregarItem.bind(this)
+    }
+
+    agregarItem(item) {
+        this.setState(prevState => {
+            return {
+                historiaCompleta: [...prevState.historiaCompleta, item]
+            }
+        })
     }
 
     render(){
+        const {opciones}= this.props;
+
        return(
        <div>
             <h1> Elige tu propia aventura </h1>
-            <HistoriaItem />
+            <HistoriaItem data={data} agregarItem={this.agregarItem} />
+            <p> Selección anterior: {opciones}</p>
+            <p> Historial de elecciones elegidas : {} </p>
+
+            <ul>
+                {this.state.historiaCompleta.map(function(item) {
+                    return (<li key={item.id}>{item.historia}</li>)
+                }
+                 )}
+            </ul>
         </div>
         )
     }
--- src/components/HistoriaItem.jsx
+++ src/components/HistoriaItem.jsx
@@ -1,62 +1,69 @@
 import React from 'react';
 import Button from "./Button";
-import data from "./data.json";
+
 
 class HistoriaItem extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data,
             numeroHistoria: 1,
             letraHistoria: "",
-            historiaActual: {},
 
         }
         this.handleClick = this.handleClick.bind(this)
     }
 
     handleClick(e) {
-        e.preventDefault()
-        //console.log(e.target.textContent)
+        e.preventDefault()        
         this.setState(prevState => {
             return {
                 ...prevState,
                 numeroHistoria: prevState.numeroHistoria + 1,
                 letraHistoria: e.target.textContent.trim()
             }
-        })
-    }
+        }, () => {
 
-    componentDidUpdate(prevProps) {
-
-        const identificador = this.state.letraHistoria;
-        console.log(identificador)
-        const siguienteHistoria = this.state.data.find(historia => historia.id == identificador)
-        if (prevProps.historiaActual !== siguienteHistoria) {
-            this.setState({ historiaActual: siguienteHistoria });
+            
+            const { agregarItem } = this.props
+            const historiaActual = this.getHistoriaActual()
+            agregarItem(historiaActual)
         }
+        
+        )
+
 
     }
+
+    getHistoriaActual() {
+
+        const { data } = this.props;
+
+        const identificador =  this.state.numeroHistoria + this.state.letraHistoria;
+        
+        const historiaActual = data.find(historia => historia.id === identificador)
+        return historiaActual;
+        
+    }
+
 
     render() {
+        console.log("render");
+
+        
+        const historiaActual = this.getHistoriaActual()
 
         return (
             <>
-                <p>
-                    {this.state.data[0].historia}
-                </p>
+                <p>{historiaActual.historia} </p>
 
                 <Button handleClick={this.handleClick} opcion="a" />
-                <p>{this.state.data[0].opciones.a}</p>
+                <p>{historiaActual.opciones.a}</p>
                 <Button handleClick={this.handleClick} opcion="b" />
-                <p>{this.state.data[0].opciones.b}</p>
+                <p>{historiaActual.opciones.b}</p>
 
             </>
         )
     }
 
 }
-
-
-
 export default HistoriaItem;
