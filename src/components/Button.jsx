e80ab4a4cd635ad66a8f7ee19957974ffcb12059
Adrianita13 <adriattar@hotmail.com>
2021-09-06 10:21:32 -0300

    lunes 6/9

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 class Button extends React.Component {
 
-   
-   
-
 
     render() {
-        const {handleClick, opcion}= this.props;
+        const {handleClick,opcion}= this.props;
+
         return (
-            <button onClick={handleClick} style={{ color: "red" }}> {opcion} </button>
+            <button onClick={handleClick} > {opcion} </button>
         )
     }
 }
--- src/components/HistoriaItem.jsx
+++ src/components/HistoriaItem.jsx
@@ -2,49 +2,61 @@ import React from 'react';
 import Button from "./Button";
 import data from "./data.json";
 
-class HistoriaItem extends React.Component{
-constructor(props){
-    super(props);
-    this.state={
-        data,
-        numeroHistoria: 1
+class HistoriaItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data,
+            numeroHistoria: 1,
+            letraHistoria: "",
+            historiaActual: {},
+
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(e) {
+        e.preventDefault()
+        //console.log(e.target.textContent)
+        this.setState(prevState => {
+            return {
+                ...prevState,
+                numeroHistoria: prevState.numeroHistoria + 1,
+                letraHistoria: e.target.textContent.trim()
+            }
+        })
+    }
+
+    componentDidUpdate(prevProps) {
+
+        const identificador = this.state.letraHistoria;
+        console.log(identificador)
+        const siguienteHistoria = this.state.data.find(historia => historia.id == identificador)
+        if (prevProps.historiaActual !== siguienteHistoria) {
+            this.setState({ historiaActual: siguienteHistoria });
+        }
+
+    }
+
+    render() {
+
+        return (
+            <>
+                <p>
+                    {this.state.data[0].historia}
+                </p>
+
+                <Button handleClick={this.handleClick} opcion="a" />
+                <p>{this.state.data[0].opciones.a}</p>
+                <Button handleClick={this.handleClick} opcion="b" />
+                <p>{this.state.data[0].opciones.b}</p>
+
+            </>
+        )
     }
 
 }
-handleClick = (e) => {
-    e.preventDefault()
-    //console.log(e.target.textContent)
-    this.setState(prevState =>{
-        return{
-             ...prevState,
-             numeroHistoria : prevState.numeroHistoria +1
-        }
-     })
-     
 
-    console.log(this.state.numeroHistoria)
-    const identificador=  this.state.numeroHistoria + e.target.textContent
-    console.log(identificador)
-    //if(e.target.textContent)
-}
 
- render(){
-
-     return(
-         <>
-         <p> 
-        {this.state.data[0].historia}
-         </p>
-
-         <Button handleClick={this.handleClick} opcion="a" />
-         <p>{this.state.data[0].opciones.a}</p>
-         <Button handleClick={this.handleClick} opcion="b" />
-         <p>{this.state.data[0].opciones.b}</p>
-    
-         </>
-     )
- }
-
-}
 
 export default HistoriaItem;
